81a2d69300c8d8b84a54dac3a7ebb2bc2885a6c6
Ígor Grillo Peternella <igor.feq@gmail.com>
2019-01-04 22:39:05 -0200

    improved game quitting code

--- jazzpy/main.py
+++ jazzpy/main.py
@@ -15,6 +15,10 @@ class JazzPy:
     SCREEN_CAPTION = "JazzPy - Jazz Jackrabbit by Epic MegaGames (1994) Remake"
 
     def __init__(self):
+        """
+        Initializes pygame, the game clock, the game running state
+        and the screen/scene manager.
+        """
         # intializes Pygame
         pygame.init()
         pygame.mixer.init()  # iniits mixer module for sound
@@ -22,7 +26,7 @@ class JazzPy:
         self.screen = self._load_screen(self.SCREEN_CAPTION)
         self.scene_manager = SceneManager()
         self.clock = pygame.time.Clock()
-        self.is_game_running = True
+        self.is_gameover = False
 
     def _load_screen(self, screen_caption):
         """
@@ -40,6 +44,27 @@ class JazzPy:
         pygame.display.set_caption(screen_caption)
 
         return screen
+
+    def _before_gameover_hook(self):
+        """
+        Executes some final code before finally closing the game.
+        """
+        self.is_gameover = True
+
+        pygame.quit()
+
+    def _check_for_gameover(self):
+        """
+        Reads Pygame's events to see if the player wants to quit or if
+        the game is over. Executes the gameover hook before closing.
+        """
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self._before_gameover_hook()
+
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self._before_gameover_hook()
 
     def _wait_for_next_frame(self):
         """
@@ -67,17 +92,16 @@ class JazzPy:
         """
         Public method that starts the main game/event loop.
         """
-        while True:
-
-            # attempts to get the quit event from the event queue
-            if pygame.event.get(pygame.QUIT):
-                break
+        while not self.is_gameover:
 
             # clock tick before next frame (fixed fps)
             self._wait_for_next_frame()
 
             # updates game state/screen by getting all events from the queue
             self._update_state(self.screen)
+
+            # attempts to get the quit event from the event queue
+            self._check_for_gameover()
 
 
 if __name__ == "__main__":
